a709f47f129c594c1c38144aa16ae056c0405b29
Krishna Chaudhari <116093539+Krishnapc04@users.noreply.github.com>
2025-02-17 17:27:36 +0530

    website url authenticated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // Parse cookies
 
-const allowedOrigins = ['https://ca.composit.in', 'https://composit.in','http://localhost:5173','https://admin.composit.in','http://localhost:3000',"https://composit-25.vercel.app"];
+const allowedOrigins = ['https://ca.composit.in', 'https://composit.in','http://localhost:5173','https://admin.composit.in','http://localhost:3000',"https://composit-25.vercel.app","https://www.composit.in/"];
 
 app.use(cors({
     origin: (origin, callback) => {
